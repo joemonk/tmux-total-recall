5ca1924f6b26ce9c48127484ce450af337986442
Matt Rooney <joemonk@gmail.com>
2026-07-29 15:10:41 +0100

    fix: delete total-recall-paste buffer after pasting to avoid polluting buffer list

--- scripts/browse.sh
+++ scripts/browse.sh
@@ -27,3 +27,4 @@ tmux show-buffer -b "$buf_name" \
     | tr -d '\n' \
     | tmux load-buffer -b total-recall-paste -
 tmux paste-buffer -p -b total-recall-paste
+tmux delete-buffer -b total-recall-paste
